5b4c912edec98a14b4992270cb3826b98e741a50
kchaudemanche <kylian.chaudemanche@gmail.com>
2018-11-26 12:04:53 +0100

    test home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 namespace App\Controller;
 
@@ -7,34 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
-=======
-/**
- * Created by PhpStorm.
- * User: kchaudemanche2018
- * Date: 26/11/2018
- * Time: 11:36
- */
-
-namespace App\Controller;
-
-
-use http\Env\Request;
-
-class MainController
->>>>>>> origin/master
 {
     /**
      * @Route("/", name="home")
      */
-<<<<<<< HEAD
-    public function home()
+    public function index()
     {
         return $this->render('main/home.html.twig');
     }
 }
-=======
-    public function home(Request $request){
-        return $this->render("main/home.html.twig");
-    }
-}
->>>>>>> origin/master
