0d8e69b75644dbeee20bc16f035780e4e8118b03
kchaudemanche <kylian.chaudemanche@gmail.com>
2018-11-27 09:03:03 +0100

    je t'envoie tout
    Signed-off-by: kchaudemanche <kylian.chaudemanche@gmail.com>

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Media;
+use App\Form\MediaType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MediaController extends Controller
@@ -21,11 +25,24 @@ class MediaController extends Controller
     /**
      * @Route("/media/create", name="media_create")
      */
-    public function createMedia()
+    public function createMedia(EntityManagerInterface $em,Request $request)
     {
-        return $this->render('media/index.html.twig', [
-            'controller_name' => 'MediaController',
-        ]);
+        $media = new Media();
+        $formIdea = $this->createForm(MediaType::class,$media);
+        $media->setAuthor($this->getUser()->getUsername());
+
+        $formIdea->handleRequest($request);
+        if($formIdea->isSubmitted() && $formIdea->isValid())
+        {
+            $idea->setDateCreated( new \DateTime());
+            $em->persist($idea);
+            $em->flush();
+
+            $this->addFlash('success',"L'idée a été ajoutée");
+
+            return $this->redirectToRoute('idea_list');
+        }
+        return $this->render('idea/formIdea.html.twig',['formIdea' => $formIdea->createView()]);
     }
 
     /**
